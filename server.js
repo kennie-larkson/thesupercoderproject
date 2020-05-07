c5c23feb6fcfd6c7a1697e415b2f5319ba3e6804
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-05-07 20:23:34 +0100

    added get and post requests

--- server.js
+++ server.js
@@ -2,31 +2,34 @@ const express = require("express")
 const port = process.env.PORT || 3000
 const bodyparser = require("body-parser")
 
-
-
 const app = express()
 
-let postData
+let postData 
 app.use(bodyparser.urlencoded({extended: true}))
 
 app.get("/",(req,res)=>{
     res.send('{status: success}')
+       
+})
+
+app.post("/data",(req,res)=>{
+
+    postData = {
+        "data": req.body.data
+    }
+
+    res.send(postData)
     
 })
 
 app.get("/data",(req,res)=>{
     res.sendFile( __dirname+'/index.html')
-    // console.log(req.body.name)
-})
-
-app.post("/data",(req,res)=>{
-    res.sendStatus(200)
-    // console.log(req.body.data)
-    postData = req.body.data
-    console.log(`{data: '${postData}'}`)
+   
+    
 })
 
 app.listen(port,()=>{
     console.log(`Server is running on port : ${port}`)
+
     
 })
